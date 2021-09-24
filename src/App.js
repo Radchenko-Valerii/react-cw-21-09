6812af11ba2c6faf8838c405164720dd00ea7e49
Valerii Radchenko <valeravivaz@gmail.com>
2021-09-24 14:37:09 +0300

    good comit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import { Component } from 'react';
-import AlohaDashboard from './components/AlohaDashboard';
+// import AlohaDashboard from './components/AlohaDashboard';
+// import StopWatch from "./components/StopWatch"
+import Form from './components/Form/Form';
 
 class App extends Component {
   render() {
-    return <AlohaDashboard />;
+  return <Form />
+  
+  
+    // return <AlohaDashboard />;
   }
 }
 
