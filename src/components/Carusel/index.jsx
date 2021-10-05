3de68360e27ffe413950c35a0c0178ac78d0da23
Valerii <valeravivaz@gmail.com>
2021-10-05 12:55:26 +0300

    add new styles and logic for carusel

--- src/components/Carusel/index.jsx
+++ src/components/Carusel/index.jsx
@@ -1,27 +1,60 @@
-import React, { Component } from 'react';
-import data from '../../Data';
-import styles from './carusel.module.scss'
+import React, { Component } from "react";
+import data from "../../Data";
+import styles from "./carusel.module.scss";
 
 class Carusel extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state ={
-      i : 0,
+    this.state = {
+      i: 0,
+      isAutoSlide: false
+    };
+  }
+
+  prevSlide = () => {
+    const { i } = this.state;
+    if (i < 1) {
+      this.setState({ i: data.length - 1 });
+    } else this.setState({ i: i - 1 });
+  };
+
+  nextSlide = () => {
+    const { i } = this.state;
+    if (i >= data.length-1) {
+      this.setState({ i: 0 });
+    } else this.setState({ i: i + 1 });
+  };
+
+
+  autoslideChanger = () =>{
+    const {isAutoSlide} = this.state
+    this.setState({isAutoSlide: !isAutoSlide})
+  }
+
+  componentDidUpdate(){
+    const int = () => setInterval(()=>{this.nextSlide()}, 1000)
+    if(this.state.isAutoSlide){
+      int()
+    } else {
+      clearInterval(int)
     }
   }
-  
+
   render() {
-    const {i} = this.state
-    const {src, description, header} = data[i]
+    const { i, isAutoSlide} = this.state;
+    const { src, description, header } = data[i];
     return (
       <div className={styles.caruselWrapper}>
         <div className={styles.imgWrapper}>
-          <button className={styles.prevBtn}>{'<'}</button>
+          <button onClick={this.prevSlide} className={styles.prevBtn}>
+            {"<"}
+          </button>
           <img className={styles.img} src={src} alt="" />
-          <button onClick={()=>{this.setState({i:i+1})}} className={styles.nextBtn}>{'>'}</button>
+          <button onClick={this.nextSlide} className={styles.nextBtn}>{">"}</button>
         </div>
+        <button onClick={this.autoslideChanger} className={styles.autoSlideBtn}>{isAutoSlide ? 'stop' : 'start'} autoslide</button>
         <h1 className={styles.header}>{header}</h1>
-          <p className={styles.description}>{description}</p>
+        <p className={styles.description}>{description}</p>
       </div>
     );
   }
