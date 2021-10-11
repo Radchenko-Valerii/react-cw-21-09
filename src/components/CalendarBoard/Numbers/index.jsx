d736df40f7456e5b4a56927b595d6dbf26ddd9d0
Valerii <valeravivaz@gmail.com>
2021-10-11 12:29:07 +0300

    add logic for days position

--- src/components/CalendarBoard/Numbers/index.jsx
+++ src/components/CalendarBoard/Numbers/index.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
-import {getMonth,  startOfToday, format, getDaysInMonth} from 'date-fns'
+import {getMonth,  startOfToday, format, getDaysInMonth, getDay, getYear} from 'date-fns'
+
 
 const Numbers = () => {
   const today = startOfToday();
   const month = getMonth(today);
+  const year = getYear(today)
   const monthNumber = format(today, 'L');
   const numbers = getDaysInMonth(Number(monthNumber))
   const daysArr = [];
   for(let i = 0; i <= numbers; i++){
     daysArr.push(i)
   }
-  const newArr = daysArr.map((day)=>{return <p >{day}</p>})
+  const newArr = daysArr.map((day)=>{return <p className={getDay(new Date(year, month, day))}>{day}</p>})
   return (
     <div style={{display:"flex", justifyContent:"space-around"}}>
       {newArr}
