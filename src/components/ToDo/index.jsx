3265d102a6f120028f012baac5dbb74525f8e327
Valerii <valeravivaz@gmail.com>
2021-10-21 00:39:53 +0300

    Add logic for changing isDone & isExist property

--- src/components/ToDo/index.jsx
+++ src/components/ToDo/index.jsx
@@ -3,69 +3,79 @@ import React from "react";
 import { useState } from "react";
 import classNames from "classnames";
 
-const data = [
-  {
-    id: 1,
-    text: "Пары - не удары, можно пропустить (с) Jason Statham",
-    isDone: false,
-  },
-];
-
 const ToDo = () => {
-  const [tasks, setTasks] = useState(data);
+  const [tasks, setTasks] = useState([]);
 
   const addTasks = (text) => {
     const newTask = {
       id: Date.now(),
       text: text,
-      isDone: false
-    }
+      isDone: false,
+      isExist: true,
+    };
     const newTasks = [...tasks, newTask];
     setTasks(newTasks);
-  }
+  };
 
   const submitHandler = (values, formikBag) => {
-    const {taskText} = values;
+    const { taskText } = values;
     addTasks(taskText);
     formikBag.resetForm();
-  }
-
+  };
+  
 
   const tasksArray = tasks.map((obj) => {
-    const checkboxClass = classNames({checked: obj.isDone, checkbox: true})
+    const checkboxClass = classNames({ checked: obj.isDone, checkbox: true });
     return (
-      <label>
-        <Field className={checkboxClass} type="checkbox" name="text" id={obj.id} defaultChecked={obj.isDone} />
-        <ol> {obj.text} </ol> 
+      <label key={obj.id}>
+        <ol>
+          <Field
+            className={checkboxClass}
+            type="checkbox"
+            name="text"
+            id={obj.id}
+            checked={obj.isDone}
+            onChange={() => {
+              obj.isDone = !obj.isDone;
+              setTasks([...tasks]);
+            }}
+          />{" "}
+          {obj.text}
+          <button
+            type="button"
+            onClick={({id}) => {
+              obj.isExist = false;
+              setTasks(tasks.filter((task) => task.isExist));
+            }}
+          >
+            delete
+          </button>
+        </ol>
       </label>
     );
   });
 
-  console.log(tasks)
+  console.log(tasks);
   return (
     <div>
       <h1>ToDo</h1>
-      <Formik initialValues={{taskText:''}} onSubmit={submitHandler}>
+      <Formik initialValues={{ taskText: "" }} onSubmit={submitHandler}>
         <Form>
-          <Field name="taskText" placeholder="enter task"/>
-          <button name="submit" type="submit">add</button>
+          <Field name="taskText" placeholder="enter task" />
+          <button name="submit" type="submit">
+            add
+          </button>
         </Form>
       </Formik>
       <ul>
-        
-        {/* {tasks.map((task)=>{return <li>{task.text}</li>})} */}
+        <Formik initialValues={{ id: "", text: "", isDone: "" }}>
+          {() => {
+            return <Form>{tasksArray}</Form>;
+          }}
+        </Formik>
       </ul>
-
-      <Formik initialValues={{id:'', text:'', isDone: ''}}>
-        {({ values }) => {
-          return <Form>{tasksArray}
-          </Form>;
-        }}
-      </Formik>
     </div>
   );
 };
 
 export default ToDo;
-
-
