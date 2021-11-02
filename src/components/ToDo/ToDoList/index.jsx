09be851107a3d3d9cfa8e0c7f303edeca664404f
Valerii <valeravivaz@gmail.com>
2021-11-02 12:29:20 +0200

    ADD ANIMATION on TRANSITION GROUP

--- src/components/ToDo/ToDoList/index.jsx
+++ src/components/ToDo/ToDoList/index.jsx
@@ -1,39 +1,49 @@
-import React from 'react';
-import { Formik, Field, Form} from "formik";
+import React from "react";
+import { Formik, Field, Form } from "formik";
 import classNames from "classnames";
-import { useContext } from 'react/cjs/react.development';
-import { ToDoContext } from '../../../contexts';
+import { useContext } from "react/cjs/react.development";
+import { ToDoContext } from "../../../contexts";
 import styles from "./toDoList.module.scss";
+import { TransitionGroup, CSSTransition } from "react-transition-group";
 
 const ToDoList = () => {
-  const [tasks, setTasks] = useContext(ToDoContext)
-
+  const [tasks, setTasks] = useContext(ToDoContext);
   const tasksArray = tasks.map((obj) => {
-    const checkboxClasses = classNames({[styles.checkbox]: true });
-    const checkboxTextClasses = classNames({ [styles.checked]: obj.isDone, [styles.checkboxText]: true })
-    const checkboxWrapperClasses = classNames({[styles.checkboxWrapper]: true, [styles.checkboxWrapperDone]: obj.isDone})
+    const checkboxClasses = classNames({ [styles.checkbox]: true });
+    const checkboxTextClasses = classNames({
+      [styles.checked]: obj.isDone,
+      [styles.checkboxText]: true,
+    });
+    const checkboxWrapperClasses = classNames({
+      [styles.checkboxWrapper]: true,
+      [styles.checkboxWrapperDone]: obj.isDone,
+    });
     return (
-      <label key={obj.id}>
+      <CSSTransition key={obj.id} timeout={500} className="transitionTask">
+      <ol>
+      <Formik initialValues={{ id: "", text: "", isDone: "" }}>
+      <Form>
+      <label>
         <div className={checkboxWrapperClasses}>
-        <ol className={checkboxClasses}>
-          <Field
-            className={styles.checkbox}
-            type="checkbox"
-            name="text"
-            id={obj.id}
-            checked={obj.isDone}
-            onChange={() => {
-              obj.isDone = !obj.isDone;
-              setTasks([...tasks]);
-            }}
-          />
-        </ol>
-        <p className={checkboxTextClasses}>{obj.text}</p>
-        <button
+          <div className={checkboxClasses}>
+            <Field
+              className={styles.checkbox}
+              type="checkbox"
+              name="text"
+              id={obj.id}
+              checked={obj.isDone}
+              onChange={() => {
+                obj.isDone = !obj.isDone;
+                setTasks([...tasks]);
+              }}
+            />
+          </div>
+          <p className={checkboxTextClasses}>{obj.text}</p>
+          <button
             className={styles.button}
             type="button"
             onClick={() => {
-              obj.isExist = false;
+              obj.isExist = !obj.isExist;
               setTasks(tasks.filter((task) => task.isExist));
             }}
           >
@@ -41,20 +51,23 @@ const ToDoList = () => {
           </button>
         </div>
       </label>
+      </Form>
+      </Formik>
+      </ol>
+      </CSSTransition>
     );
   });
+  
 
   return (
     <div>
-      <ul>
-        <Formik initialValues={{ id: "", text: "", isDone: "" }}>
-          {() => {
-            return <Form>{tasksArray}</Form>;
-          }}
-        </Formik>
-      </ul>
+      <TransitionGroup component="ul">
+          {tasksArray}
+      </TransitionGroup>
     </div>
   );
-}
+};
 
 export default ToDoList;
+
+
